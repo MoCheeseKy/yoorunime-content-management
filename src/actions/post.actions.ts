e3781c1bbbdaf1c67cedbf182c72ad75bcd595e4
Your NameMoCheeseKy <rifkyking04@gmail.com>
2026-07-21 21:56:26 +0700

    .

--- src/actions/post.actions.ts
+++ src/actions/post.actions.ts
@@ -2,7 +2,7 @@
 
 import { revalidatePath } from "next/cache";
 import prisma from "@/lib/prisma";
-import { PostStatus } from "@prisma/client";
+import { PostStatus } from "@/generated/prisma/client";
 
 export async function createPost(data: any) {
   try {
